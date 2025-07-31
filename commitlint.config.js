98f61d8ebaeb1b2fe91cb619e36e1ff5d07a6e24
Renato <askides@proton.me>
2025-07-31 13:44:27 +0100

    chore: remove release type-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [
-      2,
-      "always",
-      ["chore", "docs", "feat", "revert", "fix", "release"],
-    ],
+    "type-enum": [2, "always", ["chore", "docs", "feat", "revert", "fix"]],
     "type-case": [2, "always", "lower-case"],
     "subject-case": [2, "always", "lower-case"],
   },
