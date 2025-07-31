6907b64c4e9bb4303523bcb24c01525de918cd33
Renato <askides@proton.me>
2025-07-31 13:43:47 +0100

    chore(max): configure commit type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["chore", "docs", "feat", "revert", "fix", "release"],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "subject-case": [2, "always", "lower-case"],
+  },
+};
